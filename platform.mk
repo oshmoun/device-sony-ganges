41ca38e7cf5c4e2f27a02c18acd92ab983c27ed0
MarijnS95 <marijns95@gmail.com>
2019-03-21 20:19:09 +0100

    Revert "Disable verity."
    This reverts commit 64e04440a4251f42c7b368bd1e0bbd6de99777e9.

--- platform.mk
+++ platform.mk
@@ -215,6 +215,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.wifi.addr_path=/data/vendor/wifi/wlan_mac.bin
 
 # setup dm-verity configs.
-# PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc/c0c4000.sdhci/by-name/system
-# PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/platform/soc/c0c4000.sdhci/by-name/vendor
-# $(call inherit-product, build/target/product/verity.mk)
+PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc/c0c4000.sdhci/by-name/system
+PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/platform/soc/c0c4000.sdhci/by-name/vendor
+$(call inherit-product, build/target/product/verity.mk)
